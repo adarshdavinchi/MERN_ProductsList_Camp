4fe838511bb0e81f2f16c92d969fd542504946b1
Adarsh_Baghel <adarshdavinchi@gmail.com>
2024-09-26 05:03:16 -0700

    replace $ to sweat points

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -93,7 +93,7 @@ const ProductCard = ({ product }) => {
 				</Heading>
 
 				<Text fontWeight='bold' fontSize='xl' color={textColor} mb={4}>
-					${product.price}
+					Sweat Points: {product.price}
 				</Text>
 
 				<HStack spacing={2}>
